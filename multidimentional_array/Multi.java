2d175779804748c44cb8aba25d046ac8de76f8d6
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 13:48:10 +0530

    Multi

---
+++ multidimentional_array/Multi.java
@@ -0,0 +1,8 @@
+public class Multi{
+    public static void main(String args[]){
+        int a[][] = {{10,11,13},{14,15,16},{20,30,50}};
+        System.out.println(a[1][2]);
+        System.out.println(a[2][3]);
+        System.out.println(a[3][1]);
+    }
+}
