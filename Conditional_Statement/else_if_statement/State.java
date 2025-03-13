445af72364b4223b2fca515d2ad60fa3f91b65d6
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 11:57:04 +0530

    Posi

---
+++ Conditional_Statement/else_if_statement/State.java
@@ -0,0 +1,15 @@
+public class State{
+    public static void main(String args[]){
+        int a = 22;
+        if(a>0){
+            System.out.println("Positive");
+        }
+        else if(a<0){
+            System.out.println("Negative");
+        }
+        else{
+            System.out.println("The no.is zero");
+             
+        }
+    }
+}
