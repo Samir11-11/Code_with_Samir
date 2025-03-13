d836c35a71a652d28679cc1f42fb8868ded194cc
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 15:11:36 +0530

    Age

---
+++ Next_int/Age.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Age{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Age");
+        int name = input.nextInt();
+        System.out.println("Your age is:");
+    }
+}
