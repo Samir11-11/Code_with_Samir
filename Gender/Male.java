aceb8014796842e293e32e3f2116c68b89e171cf
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 11:42:17 +0530

    Gender

---
+++ Gender/Male.java
@@ -0,0 +1,12 @@
+public class Male{
+    public static void main(String args[]){
+        String a = "M";
+        if(a=="M"){
+            System.out.println("Male");
+        }
+        else{
+            System.out.println("Female");
+             
+        }
+    }
+}
