d6365f57b663f8d0178d199e534a027222b5aff9
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 15:17:55 +0530

    Adding

---
+++ dynamic_addition/Dyna.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Dyna{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("First No.");
+        int n = name.nextInt();
+        System.out.println("Second No.");
+        int m = name.nextInt();
+        int a = n + m;
+        System.out.println("The sum is" + a);
+    }
+}
