e0bdc12ded17034520ae0c3e7a3755818820d1cb
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 11:28:47 +0530

    Negative

---
+++ Finding_negative/Nega.java
@@ -0,0 +1,11 @@
+public class Nega{
+    public static void main(String args[]){
+        int a=15;
+        if (a>10){
+        System.out.println("Negative");
+        }
+        else{
+            System.out.println("Positive");
+        }
+    }
+}
