72d80f662cc2a805793b348c467981b8978ca30e
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 11:11:44 +0530

    Vote

---
+++ major_or_minor_finder/Voter.java
@@ -0,0 +1,11 @@
+public class Voter{
+    public static void main(String args[]){
+        int a=15;
+        if (a>10){
+        System.out.println("minor");
+        }
+        else{
+            System.out.println("major");
+        }
+    }
+}
