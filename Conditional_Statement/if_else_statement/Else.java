eb50cf1452774f8c7a6c359d12f5733dcadd610f
Samir11-11 <samirnongmai@gmail.com>
2025-03-13 11:00:04 +0530

    Else

---
+++ Conditional_Statement/if_else_statement/Else.java
@@ -0,0 +1,11 @@
+public class Else{
+    public static void main(String args[]){
+        if(true){
+            System.out.println("poirei is gay");
+        }
+        else{
+            System.out.println("who is she");
+             
+        }
+    }
+}
