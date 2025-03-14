f0133277e7345997262d8c8d77d69a12c7e94615
Samir11-11 <samirnongmai@gmail.com>
2025-03-14 12:06:30 +0530

    Index

---
+++ index/Index.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Split{
+    public static void main(String args[]){
+        String name = "Purnima,dolly";
+        
+        System.out.println(name.indexOf("d"));
+    }
+}
