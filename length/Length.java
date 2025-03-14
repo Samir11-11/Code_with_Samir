07fa4cccd9dcee9d65b28c0b5a205147f38a544b
Samir11-11 <samirnongmai@gmail.com>
2025-03-14 11:54:35 +0530

    Length

---
+++ length/Length.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Length{
+    public static void main(String args[]){
+        String name = "Purnima, Dolly";
+        
+        System.out.println(name.length());
+    }
+}
