f76a56ebf1741d91985709a7718eb613d3abc786
Samir11-11 <samirnongmai@gmail.com>
2025-03-14 11:48:50 +0530

    Split

---
+++ split/Split.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Split{
+    public static void main(String args[]){
+        String name = "Purnima, Dolly";
+        String names[] = name.split(",");
+        System.out.println(names[0]);
+    }
+}
